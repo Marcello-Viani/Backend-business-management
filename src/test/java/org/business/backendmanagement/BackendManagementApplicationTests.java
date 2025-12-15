a290c08454a815710e741b4ee82be23855af4177
MarcelloViani <alemarci81@gmail.com>
2025-12-15 22:32:34 +0100

    Initialize Spring Boot project structure

---
+++ src/test/java/org/business/backendmanagement/BackendManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.business.backendmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
